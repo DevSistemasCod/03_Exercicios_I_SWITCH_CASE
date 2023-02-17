b6b8fd227afc160bd9b23c3aca794d38295d48ce
DevSistemasCod <124713140+DevSistemasCod@users.noreply.github.com>
2023-02-17 14:14:47 -0300

    Add files via upload

--- ClassificaSeVogal.java
+++ ClassificaSeVogal.java
@@ -8,41 +8,23 @@ public class ClassificaSeVogal {
 	/*Faça um programa em Java que leia uma letra, 
 	 * escreva na tela se essa letra é uma vogal ou consoante 
 	 * (considerar apenas letras minúsculas).
-	 * versao 1.0
+	 * versao 2.0
 	 * */	
 		char letra;
 		Scanner lerTeclado = new Scanner(System.in);
-	    	System.out.println("Informe uma letra: ");
+	    System.out.println("Informe uma letra: ");
 		letra = lerTeclado.next().charAt(0);
 		
 		switch (letra) {
 		case 'a':
-			System.out.println("Vogal");
-			break;
 		case 'A':
-			System.out.println("Vogal");
-			break;
 		case 'e':
-			System.out.println("Vogal");
-			break;
 		case 'E':
-			System.out.println("Vogal");
-			break;
 		case 'i':
-			System.out.println("Vogal");
-			break;
 		case 'I':
-			System.out.println("Vogal");
-			break;
 		case 'o':
-			System.out.println("Vogal");
-			break;
 		case 'O':
-			System.out.println("Vogal");
-			break;
 		case 'u':
-			System.out.println("Vogal");
-			break;
 		case 'U':
 			System.out.println("Vogal");
 			break;
