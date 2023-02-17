72e77ef5fe488fc917ada099fbbb7eda64b5685a
DevSistemasCod <124713140+DevSistemasCod@users.noreply.github.com>
2023-02-17 14:10:21 -0300

    Add files via upload

---
+++ ClassificaSeVogal.java
@@ -0,0 +1,54 @@
+package casosComSwitch;
+
+import java.util.Scanner;
+
+public class ClassificaSeVogal {
+
+	public static void main(String[] args) {
+	/*Faça um programa em Java que leia uma letra, 
+	 * escreva na tela se essa letra é uma vogal ou consoante 
+	 * (considerar apenas letras minúsculas).
+	 * */	
+		char letra;
+		Scanner lerTeclado = new Scanner(System.in);
+	    System.out.println("Informe uma letra: ");
+		letra = lerTeclado.next().charAt(0);
+		
+		switch (letra) {
+		case 'a':
+			System.out.println("Vogal");
+			break;
+		case 'A':
+			System.out.println("Vogal");
+			break;
+		case 'e':
+			System.out.println("Vogal");
+			break;
+		case 'E':
+			System.out.println("Vogal");
+			break;
+		case 'i':
+			System.out.println("Vogal");
+			break;
+		case 'I':
+			System.out.println("Vogal");
+			break;
+		case 'o':
+			System.out.println("Vogal");
+			break;
+		case 'O':
+			System.out.println("Vogal");
+			break;
+		case 'u':
+			System.out.println("Vogal");
+			break;
+		case 'U':
+			System.out.println("Vogal");
+			break;
+		default:
+			System.out.println("Consoante");
+		}
+		
+	}
+
+}
